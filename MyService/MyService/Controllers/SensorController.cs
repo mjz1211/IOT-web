5e7afb164a24e7db858ea4e1741aa72ddc854ad1
l504 <l504@L504-P019.cht.com.tw>
2018-10-24 16:25:10 +0800

    first commit

---
+++ MyService/MyService/Controllers/SensorController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace MyService.Controllers
+{
+    public class SensorController : ApiController
+    {
+        //proxy 配置 device 到 sensor 最後感測資料
+        [HttpGet]
+        [RouteAttribute("cht/Sensor/sensorLastData/deviceid/{deviceid}/sensor/{sensorid}")]
+        public async Task<String> sensorLastData([FromUri]String deviceid, [FromUri]String sensorid)
+        {
+            //取得 Hosted
+            String hostName = Models.AppKeyUtility.getKeyValue("hosted");
+            String endPoint = String.Format($"{hostName}/device/{deviceid}/sensor/{sensorid}/rawdata");
+
+            WebClient client = new WebClient();
+            //header CK key
+            client.Headers.Add("CK", "DKGM95TKZK3FYUKKEU");
+            //callback delegate (事件委派)
+            client.DownloadStringCompleted += downloadComplete;
+
+            //client.DownloadStringAsync(new Uri(endPoint));
+
+            String result = await client.DownloadStringTaskAsync(endPoint);
+
+            return endPoint;
+        }
+        //事件程序
+        private void downloadComplete(object sender, DownloadStringCompletedEventArgs e)
+        {
+            String jsonString = e.Result;
+        }
+    }
+}
---
+++ MyService/MyService/Models/AppKeyUtility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyService.Models
+{
+    public class AppKeyUtility
+    {
+        public static String getKeyValue(String keyName)
+        {
+            return System.Web.Configuration.WebConfigurationManager.AppSettings[keyName];
+        }
+    }
+}
